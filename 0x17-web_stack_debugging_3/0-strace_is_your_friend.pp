ec553a2ecd86d7a0db21f4b65daa5519dea865f9
John Nyokabi <jwnyokabi@gmail.com>
2023-05-10 19:56:53 +0300

    web stack debugging #3

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,3 @@
 #fix a container when a GET HTTP method is requested to Apache web server
 
-exec {'replace':
-     provider => shell,
-     command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php'
-}
+exec { '/usr/bin/env sed -i "s/phpp/php/g" /var/www/html/wp-settings.php': }
