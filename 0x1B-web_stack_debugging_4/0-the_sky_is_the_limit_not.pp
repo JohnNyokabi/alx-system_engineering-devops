6960e5d7e0e5af8a1a6189224aa83bc01c0d3569
John Nyokabi <jwnyokabi@gmail.com>
2023-05-18 18:55:36 +0300

    webstack debugging #4

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,10 +1,10 @@
 #fix the stack to have 0 bugs
 exec {'replace':
-	provider => shell,
-	command	 => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
-	before	 => Exec['restart'],
+     provider => shell,
+     path     => '/bin',
+     command	 => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx'
 }
 exec {'restart':
-	provider => shell,
-	command	 => 'sudo service nginx restart',
+     provider => shell,
+     command	 => 'sudo service nginx restart',
 }
