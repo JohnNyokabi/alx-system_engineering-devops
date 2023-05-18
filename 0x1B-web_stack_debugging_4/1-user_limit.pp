d7aa46ab5a5ee23fac637acdc445e801c3cf26cd
John Nyokabi <jwnyokabi@gmail.com>
2023-05-17 21:59:16 -0700

    Revert "web stack debugging#4"
    This reverts commit 0ab5bed0d4881cac95618ff0295b9f750cd63b8b.

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,11 +1,12 @@
 # change OS configuration to login with holberton user
 
 exec {'replace-1':
+	provider => shell,
 	command	 => 'sudo sed -i "s/nofile 5/nofile 50000/" /etc/security/limits.conf',
-	path	 => '/usr/local/bin/:/bin/'
+	before	 => Exec['replace-2'],
 }
 
 exec {'replace-2':
-	comand	 => 'sudo sed -i "s/nofile 4/nofile 50000/" /security/limits.config',
-	path 	 => '/usr/local/bin/:/bin/'
+	provider => shell,
+	comand	 => 'sudo sed -i "s/noile 4/nofile 40000/" /security/limits.config',
 }
