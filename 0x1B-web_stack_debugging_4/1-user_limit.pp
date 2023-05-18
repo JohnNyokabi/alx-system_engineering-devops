00273bd6f4c626257bb2eb44c1e7e77a4fc4f58c
John Nyokabi <jwnyokabi@gmail.com>
2023-05-18 18:58:40 +0300

    webstack debugging #4

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,12 +1,13 @@
 # change OS configuration to login with holberton user
 
 exec {'replace-1':
-	provider => shell,
-	command	 => 'sudo sed -i "s/nofile 5/nofile 50000/" /etc/security/limits.conf',
-	before	 => Exec['replace-2'],
+     provider => shell,
+     command	 => 'sudo sed -i "s/nofile 5/nofile 50000/" /etc/security/limits.conf',
+     path	 => '/bin'
 }
 
 exec {'replace-2':
-	provider => shell,
-	comand	 => 'sudo sed -i "s/noile 4/nofile 40000/" /security/limits.config',
+     provider => shell,
+     comand	 => 'sudo sed -i "s/noile 4/nofile 40000/" /security/limits.config',
+     path	 => '/bin'
 }
