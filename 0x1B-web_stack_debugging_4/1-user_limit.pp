e428d263e31e04315480c3bb2118371963199859
John Nyokabi <jwnyokabi@gmail.com>
2023-05-20 11:05:43 +0300

    Web stack debugging #4

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,11 +1,11 @@
 # change OS configuration to login with holberton user
 
 exec { 'hard limit':
-     provider	=> shell,
-     command	=> 'sudo sed -i "s/nofile 5/nofile 4000/" /etc/security/limits.conf',
-     before	=> Exec['soft limit'],
+  provider => shell,
+  command  => 'sudo sed -i "s/nofile 5/nofile 4000/" /etc/security/limits.conf',
+  before   => Exec['soft limit'],
 }
 exec { 'soft limit':
-     provider => shell,
-     command  => 'sudo sed -i "s/nofile 4/nofile 2000/" /etc/security/limits.conf'
+  provider => shell,
+  command  => 'sudo sed -i "s/nofile 4/nofile 2000/" /etc/security/limits.conf'
 }
